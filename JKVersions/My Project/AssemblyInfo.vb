f18880c6c5a2d25302b64cc7b42a9358d30c9d88
megazzt <megazzt@gmail.com>
2015-01-31 12:15:32 -0500

    Committed the files in my project. IIRC VS uses absolute paths in its project files so it might take a little elbow grease on the .sln and .vbproj files to get it working.
    Originally compiled for .NET 2 but on the latest VS that doesn't seem to
    work (and new Windows have 2 turned off by default anyway) so now it is
    set to compile for 4.

---
+++ JKVersions/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("JKVersions")> 
+<Assembly: AssemblyDescription("Downloads JK v1.01 and patches it to 1.0 and the unofficial patch.")> 
+<Assembly: AssemblyCompany("Dan Bugglin")> 
+<Assembly: AssemblyProduct("JKVersions")> 
+<Assembly: AssemblyCopyright("Copyright © 2009-Present Dan Bugglin")> 
+'<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e7d47b2c-2261-4cb3-b1fa-f87f7c748a3d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.0.3")> 
+<Assembly: AssemblyFileVersion("1.1.0.3")> 
